6300a00ac277ac6198b44d4e6de985f6b0697c5e
Mickey <micksheridan.24@gmail.com>
2019-04-30 09:31:03 -0400

    submit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+const NavBar = () => {
+  return (
+    <div>
+      <NavLink to="/" exact style={link} activeStyle={{ background: "darkblue" }}>
+        Home
+      </NavLink>
+      <NavLink to="/about" exact style={link} activeStyle={{ background: "darkblue" }}>
+        About
+      </NavLink>
+      <NavLink to="/login" exact style={link} activeStyle={{ background: "darkblue" }}>
+        Login
+      </NavLink>
+    </div>
+  );
+};
 const Home = () => {
   return (
     <div>
@@ -9,7 +33,32 @@ const Home = () => {
   );
 };
 
+const About = () => {
+  return (
+    <div>
+      <h1>Component Time</h1>
+    </div>
+  );
+};
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>This app uses the honor system</div>
+      </form>
+    </div>
+  );
+};
+
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <React.Fragment>
+      <NavBar />
+      <Route exact path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+    </React.Fragment>
+  </Router>,
+  document.getElementById("root"),
 );
